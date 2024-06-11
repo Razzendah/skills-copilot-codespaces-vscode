9e1d535239b1fff60020d2b0f101145eeccb9909
Razzendah <jmpamm_01@hotmail.com>
2024-06-11 19:18:58 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+    return {
+  skills: ['JavaScript', 'React', 'Node.js', 'MongoDB', 'Express.js', 'HTML', 'CSS', 'Sass'],
+  bio: 'I am a full stack developer with experience in JavaScript, React, Node.js, MongoDB, Express.js, HTML, CSS, and Sass.',
+};
+}
